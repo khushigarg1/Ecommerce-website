23d5d16a3e9818fd8c7f22c8adccef6a6a9fc470
khushigarg1 <khushigarg.64901@gmail.com>
2022-11-30 01:55:21 +0530

    signin

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,15 @@
-import { signInWithGooglePopup } from "../../utils/firebase.utils";
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth
+} from "../../utils/firebase.utils";
 
 const SignIn = () => {
+
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
     };
+
     return (
         <div>
             <h1>Sign In</h1>
